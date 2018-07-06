2296bba88c8a83daccf26d145fd66c578ab2df8a
Hiroshi Yamasaki <hiroshi_yamasaki@eastback.jp>
2018-07-06 18:13:44 +0900

    トップページのレイアウトおよびバグを修正しました。 #73

--- app/assets/javascripts/themes/ideas/form.js
+++ app/assets/javascripts/themes/ideas/form.js
@@ -2,8 +2,11 @@ IdeaForm = function() {}
 
 IdeaForm.prototype.render = function() {
   $(".idea-add-new").off('click');
-  $(".idea-add-new").on('click', function(){ $('.idea-form').slideToggle(100); });
+  $(".idea-add-new").on('click', function(){
+    $('.idea-form').slideToggle(100);
+  });
 
+  $(".idea-add-close").off('click');
   $(".idea-add-close").on('click', function(){ $('.idea-form').slideToggle(100); });
 };
 
